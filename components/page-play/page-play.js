89567a48f25de0409e6e0eb79e7a30a2daf4cd10
JingAn Chen <chen.jingan@icloud.com>
2021-05-03 00:42:45 +0800

    More widgets and layout

--- components/page-play/page-play.js
+++ components/page-play/page-play.js
@@ -1,6 +1,7 @@
 import "../widget-multiple-choices/widget-multiple-choices.js";
 import "../widget-video-player/widget-video-player.js";
 import "../widget-click-and-play/widget-click-and-play.js";
+import "../widget-hotzone/widget-hotzone.js";
 
 import ImportTemplateContentElement from "../import-template-content-element/import-template-content-element.js";
 
@@ -15,11 +16,6 @@ window.customElements.define(
 			super.connectedCallback();
 			this.querySelector("nav.next").addEventListener("click", (e) => this.next());
 			this.querySelector("nav.prev").addEventListener("click", (e) => this.prev());
-			for (const widget of this.querySelectorAll(":scope > main > section > *")) {
-				if (widget.hasAttribute("data-order")) {
-					widget.style.transitionDelay = `${0.5 * parseInt(widget.getAttribute("data-order"))}s`;
-				}
-			}
 		}
 
 		next() {
