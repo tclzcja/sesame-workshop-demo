70fca342cf5db81529c88169e3f66243a4cc0ebe
JingAn Chen <chen.jingan@icloud.com>
2021-05-02 22:51:01 +0800

    Minor style update

--- components/page-play/page-play.js
+++ components/page-play/page-play.js
@@ -13,6 +13,29 @@ window.customElements.define(
 
 		connectedCallback() {
 			super.connectedCallback();
+			this.querySelector("nav.next").addEventListener("click", (e) => this.next());
+			this.querySelector("nav.prev").addEventListener("click", (e) => this.prev());
+			for (const widget of this.querySelectorAll(":scope > main > section > *")) {
+				if (widget.hasAttribute("data-order")) {
+					widget.style.transitionDelay = `${0.5 * parseInt(widget.getAttribute("data-order"))}s`;
+				}
+			}
+		}
+
+		next() {
+			const currentSection = this.querySelector(":scope > main > section.on");
+			if (currentSection.nextElementSibling) {
+				currentSection.classList.remove("on");
+				currentSection.nextElementSibling.classList.add("on");
+			}
+		}
+
+		prev() {
+			const currentSection = this.querySelector(":scope > main > section.on");
+			if (currentSection.previousElementSibling) {
+				currentSection.classList.remove("on");
+				currentSection.previousElementSibling.classList.add("on");
+			}
 		}
 	}
 );
